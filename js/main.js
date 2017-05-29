d055da539b03de837956b7772d506e3b96710205
Wilmer Blanco <wilmervzla2000@gmail.com>
2017-05-29 02:01:02 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+	var shrinkHeader = 350; 
+	$(window).scroll(function () {
+	    var scroll = getCurrentScroll();
+        if (scroll >= shrinkHeader) {
+            $('header').addClass('stick');
+        }
+        else {
+            $('header').removeClass('stick');
+        }
+	});
+	function getCurrentScroll() {
+		return window.pageYOffset || document.documentElement.scrollTop;
+	}
+});
 
